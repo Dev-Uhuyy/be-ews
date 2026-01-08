1bad45f20991a36847cf92337ee8513321e9d86e
jovansantosa <jovansantosa08@gmail.com>
2026-01-08 10:00:01 +0700

    prodi_user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             DosenSeeder::class,
             MahasiswaSeeder::class,
+            ProdiUserSeeder::class,
         ]);
     }
 }
